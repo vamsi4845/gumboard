2cfabbd2d20b028f789daef7fd7ced6d9295e74e
Ershad Kunnakkadan <ershad92@gmail.com>
2025-06-28 12:24:11 -0400

    Configure Resend provider

--- auth.ts
+++ auth.ts
@@ -1,5 +1,20 @@
 import NextAuth from "next-auth"
- 
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import Resend from "next-auth/providers/resend"
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    Resend({
+      from: "noreply@gumboard.com",
+    }),
+  ],
+  pages: {
+    signIn: "/auth/signin",
+    verifyRequest: "/auth/verify-request",
+    error: "/auth/error",
+  },
 })
